3d2ad675dd7f71722e3a19ac82e2dadc508af5e4
Adrian Apostol <adrianapostol89@gmail.com>
2023-01-25 18:20:15 +0200

    Formatted a bit

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -11,7 +11,7 @@ dashboard.custom_header = {
 }
 
 dashboard.custom_center = {
-  { icon = '  ', desc = 'New file                       ', action = 'enew' },
+  { icon = '  ', desc = 'New file                                      ', action = 'enew' },
   { icon = '  ', shortcut = 'SPC f', desc = 'Find file                 ', action = 'Telescope find_files' },
   { icon = '  ', shortcut = 'SPC h', desc = 'Recent files              ', action = 'Telescope oldfiles' },
   { icon = '  ', shortcut = 'SPC g', desc = 'Find Word                 ', action = 'Telescope live_grep' },
